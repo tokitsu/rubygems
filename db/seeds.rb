7099bb7b3d06d4cb507d4d69edd8afa5942a4b3b
ishin.tokitsu <ishin.tokitsu@ctc-g.co.jp>
2020-09-08 23:53:26 +0900

    add columns to coures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: 'momo@mail.com', password: 'momomo', password_confirmation: 'momomo')
+#User.create!(email: 'momo@mail.com', password: 'momomo', password_confirmation: 'momomo')
 
 30.times do
     Course.create!([{
       title: Faker::Educator.course_name,
       description: Faker::TvShows::GameOfThrones.quote,
-      user_id: User.first.id
+      user_id: User.first.id,
+      short_description: Faker::Quote.famous_last_words,
+      level: 'Beginner',
+      language: Faker::ProgrammingLanguage.name,
+      price: Faker::Number.between(from: 1000, to: 2000)
     }])
 end
